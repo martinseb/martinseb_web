00b29a245c8bd0e57a8510c002b47020e3aed14b
Sebastian Martinez <s.martinezgd@gmail.com>
2019-02-03 16:21:20 -0500

    Added firebase support for deployment

---
+++ src/components/UnderConstruction.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class UnderConstruction extends Component {
+  render() {
+    return (
+      <div className="UnderConstruction">
+        <h1>Martin will be back soon.</h1>
+        <p>He's creating an awesome website for himself.</p>
+      </div>
+    );
+  }
+}
